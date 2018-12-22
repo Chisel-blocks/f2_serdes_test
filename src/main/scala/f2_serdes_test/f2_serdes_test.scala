8eee8eb0dae371c19879b9b043a4a33f7392ef56
Marko Kosunen <marko.kosunen@aalto.fi>
2018-12-21 17:15:24 -0800

    Bugfix in f2_serdes_test
    Some of the state transitions seemed to be buggy

--- src/main/scala/f2_serdes_test/f2_serdes_test.scala
+++ src/main/scala/f2_serdes_test/f2_serdes_test.scala
@@ -9,9 +9,9 @@ import dsptools._
 import dsptools.numbers._
 import f2_rx_dsp._
 import edge_detector._
-import memblock._ 
+import memblock._
 
-class serdes_test_scan_ios[T<: Data]( proto: T, val memsize: Int) 
+class serdes_test_scan_ios[T<: Data]( proto: T, val memsize: Int)
     extends Bundle {
         val write_mode   = Input(UInt(2.W))
         val write_address= Input(UInt(log2Ceil(memsize).W))
@@ -35,14 +35,14 @@ class f2_serdes_test_io[T <: Data](
 
 class f2_serdes_test[T <:Data] (
         proto            : T,
-        n                : Int=16, 
+        n                : Int=16,
         users            : Int=4,
         memsize          : Int=scala.math.pow(2,13).toInt
     ) extends Module {
-    val io = IO( 
+    val io = IO(
         new f2_serdes_test_io(
             proto     = proto,
-            memsize   = memsize 
+            memsize   = memsize
         )
     )
     //val z = new usersigzeros(n=n, users=users)
@@ -59,8 +59,8 @@ class f2_serdes_test[T <:Data] (
     io.to_serdes.bits:=iofifozero
     io.to_serdes.valid:=true.B
     io.from_serdes.ready:=false.B
-  
-    // Need a memory with write from scan, read to scan, and 
+
+    // Need a memory with write from scan, read to scan, and
     // To map this to SRAM, write address must be syncroniozed
     // All addressing through write_addri, enables throuhg write_en
     //val proto=new iofifosigs(n=16, users=4)
@@ -74,14 +74,14 @@ class f2_serdes_test[T <:Data] (
      OBS: The read and write modes of the scan_test_mem may be discrepant
      It is up to programmer if the mode combinations are rational
     **/
-   
+
     //Write mode mapping
     val zero :: scan :: fill :: loop :: flush:: Nil = Enum(5)
 
     val w_write_mode=Wire(UInt())
     val fill_edge= Module ( new edge_detector()).io
     val write_loop_edge= Module ( new edge_detector()).io
-     
+
     //Defaults
     fill_edge.A:=false.B
     write_loop_edge.A:=false.B
@@ -94,81 +94,82 @@ class f2_serdes_test[T <:Data] (
     } .elsewhen (io.scan.write_mode===2.U ) {
         fill_edge.A:=true.B
     } .elsewhen (io.scan.write_mode===3.U ) {
-        w_write_mode := loop 
+        w_write_mode := loop
     } .otherwise {
         w_write_mode := zero
     }
-    
+
     // State registers
     val write_state= RegInit(zero) //state register
     val read_state= RegInit(zero)
 
     //Write state machine
     val write_count=RegInit(0.U(memsize.W))
-    when( w_write_mode===fill ) { fill_edge.A===true.B }.otherwise { fill_edge.A===false.B } 
-    when( w_write_mode===loop ) { 
-        write_loop_edge.A===true.B 
-    }.otherwise { 
-        write_loop_edge.A===false.B 
-    } 
-   
+    when( w_write_mode===fill ) { fill_edge.A===true.B }.otherwise { fill_edge.A===false.B }
+    when( w_write_mode===loop ) {
+        write_loop_edge.A===true.B
+    }.otherwise {
+        write_loop_edge.A===false.B
+    }
+
     when (write_state===zero) {
         when { fill_edge.rising===true.B }{
             write_state:=fill
         }.elsewhen{ write_loop_edge.rising===true.B}{
             write_state:=loop
-        }.otherwise{ 
+        }.otherwise{
             write_state:=w_write_mode
         }
         io.from_serdes.ready:=false.B
         write_count:=0.U(memsize.W)
 
     }.elsewhen(write_state===scan) {
-        when( io.scan.write_en===true.B) { 
+        when( io.scan.write_en===true.B) {
             mem.write_addr:=io.scan.write_address
-            mem.write_val:=io.scan.write_value 
+            mem.write_val:=io.scan.write_value
         }
         write_state:=w_write_mode
     }.elsewhen(write_state===fill) {
         io.from_serdes.ready:=true.B
         when ( write_count < memsize) {
             mem.write_addr:=write_count
-            mem.write_val:=io.from_serdes.bits 
+            mem.write_val:=io.from_serdes.bits
             write_count:=write_count+1.U
             write_state:=write_state
         }.otherwise {
             write_count:=0.U(memsize.W)
-            write_state := zero //Return to zero state
+            write_state := zero //Return to zero state after fill
         }
     }.elsewhen(write_state === loop ) {
         io.from_serdes.ready:=true.B
+        write_state := w_write_mode
         when (read_state === loop) {
+            //read state update by read state machine
             when ( write_count < memsize) {
                 io.to_serdes.valid:=RegNext(RegNext(true.B))
                 mem.read_addr:=write_count+1.U
                 io.to_serdes.bits:=mem.read_val
                 mem.write_addr:=write_count
-                mem.write_val:=io.from_serdes.bits 
+                mem.write_val:=io.from_serdes.bits
                 write_count:=write_count+1.U
             }.otherwise {
                 io.to_serdes.valid:=RegNext(RegNext(true.B))
                 mem.read_addr:=write_count+1.U
                 io.to_serdes.bits:=mem.read_val
                 mem.write_addr:=write_count
-                mem.write_val:=io.from_serdes.bits 
+                mem.write_val:=io.from_serdes.bits
                 write_count:=0.U(memsize.W)
             }
         }.otherwise {
             when ( write_count < memsize) {
                 mem.write_addr:=write_count
-                mem.write_val:=io.from_serdes.bits 
+                mem.write_val:=io.from_serdes.bits
                 write_count:=write_count+1.U
             }.otherwise {
                 write_count:=0.U(memsize.W)
                 write_count:=write_count+1.U
             }
         }
-        write_state := w_write_mode 
     }.otherwise{
             //We should not end up here
             write_count:=0.U(memsize.W)
@@ -183,7 +184,7 @@ class f2_serdes_test[T <:Data] (
     //Defaults
     flush_edge.A:=false.B
     read_loop_edge.A:=false.B
-     
+
     //Read mode mapping
     when (io.scan.read_mode===0.U) {
         w_read_mode := zero
@@ -192,15 +193,15 @@ class f2_serdes_test[T <:Data] (
     } .elsewhen (io.scan.read_mode===2.U ) {
         w_read_mode := flush
     } .elsewhen (io.scan.read_mode===3.U ) {
-        w_read_mode := loop 
+        w_read_mode := loop
     } .otherwise {
         w_read_mode := zero
     }
-    
+
     //Read state machine
     val read_count=RegInit(0.U(memsize.W))
-    when( w_read_mode===flush ) { flush_edge.A===true.B }.otherwise { flush_edge.A===false.B } 
-    when( w_read_mode===loop ) { read_loop_edge.A===true.B }.otherwise { read_loop_edge.A===false.B } 
+    when( w_read_mode===flush ) { flush_edge.A===true.B }.otherwise { flush_edge.A===false.B }
+    when( w_read_mode===loop ) { read_loop_edge.A===true.B }.otherwise { read_loop_edge.A===false.B }
     when (read_state===zero) {
         when { flush_edge.rising===true.B }{
             read_count:=0.U(memsize.W)
@@ -208,17 +209,17 @@ class f2_serdes_test[T <:Data] (
         }.elsewhen{ read_loop_edge.rising===true.B}{
             read_count:=1.U(memsize.W)
             read_state:=loop
-        }.otherwise{ 
+        }.otherwise{
             read_count:=0.U(memsize.W)
             read_state:=w_read_mode
             io.to_serdes.valid:=false.B
         }
     }.elsewhen(read_state===scan) {
-        when( io.scan.read_en===true.B) { 
+        when( io.scan.read_en===true.B) {
             mem.read_addr:=io.scan.read_address
-            io.scan.read_value:=mem.read_val 
+            io.scan.read_value:=mem.read_val
         }
-        read_state:=read_state
+        read_state:=w_read_mode
     }.elsewhen(read_state===flush) {
         when ( read_count < memsize) {
             mem.read_addr:=read_count
@@ -228,33 +229,32 @@ class f2_serdes_test[T <:Data] (
             read_state:=read_state
         }.otherwise {
             read_count:=0.U(memsize.W)
-            read_state := zero //return to zero state 
+            read_state := zero //return to zero state
         }
     }.elsewhen(read_state === loop) {
+        read_state:=w_read_mode
         when(write_state===loop) {
             //both tasks taken into account in write loop
             read_count:=0.U(memsize.W)
-            read_state := w_read_mode 
         }.otherwise {
             when ( read_count < memsize) {
                 io.to_serdes.valid:=RegNext(RegNext(true.B))
                 io.to_serdes.bits :=mem.read_val
                 read_count:=read_count+1.U
-                read_state:=read_state
             }.otherwise {
                 io.to_serdes.valid:=RegNext(RegNext(true.B))
                 io.to_serdes.bits :=mem.read_val
                 read_count:=0.U(memsize.W)
-                read_state:=read_state
             }
         }
     }.otherwise{
             read_count:=0.U(memsize.W)
             //We should not end up here
-            read_state := zero
+            // Controlled by scan
+            read_state := w_read_mode
             io.to_serdes.valid:=false.B
     }
-   
+
 }
 //This gives you verilog
 object f2_serdes_test extends App {
