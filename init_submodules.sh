e25581f50530eb8f7f8e1cdcfa42143a41c0028e
Marko Kosunen <marko.kosunen@aalto.fi>
2019-04-24 18:56:31 -0700

    Remove rocket chip submodule

--- init_submodules.sh
+++ init_submodules.sh
@@ -1,22 +1,17 @@
-#!/bin/sh
-#Init submodules in this dir, if any
-DIR="$( cd "$( dirname $0 )" && pwd )"
-git submodule update --init
-
 #Dependency satisfied by submodule f2_rx_path
 #Publish local the ones you need
-cd $DIR/rocket-chip
-git submodule update --init firrtl
-git submodule update --init chisel3
-git submodule update --init hardfloat
-
-cd $DIR/rocket-chip/firrtl
-sbt publishLocal
-cd $DIR/rocket-chip/chisel3
-sbt publishLocal
-
-cd $DIR/rocket-chip
-sbt publishLocal
+#cd $DIR/rocket-chip
+#git submodule update --init firrtl
+#git submodule update --init chisel3
+#git submodule update --init hardfloat
+#
+#cd $DIR/rocket-chip/firrtl
+#sbt publishLocal
+#cd $DIR/rocket-chip/chisel3
+#sbt publishLocal
+#
+#cd $DIR/rocket-chip
+#sbt publishLocal
 
 #Prog_delay and clkmux are included in f2_rx_path
 SUBMODULES="\
